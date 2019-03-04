e288519c7bc7d4544e4d15b4c52f6238bb239a16
http://www.github.com/mimsbot <mimseyfield@gmail.com>
2019-03-04 13:57:44 -0500

    first commit

---
+++ contact.py
@@ -0,0 +1,24 @@
+from peewee import SqliteDatabase, Model, CharField, TextField
+
+db = SqliteDatabase('crm.db')
+
+
+class Contact(Model):
+
+    first_name = CharField()
+    last_name = CharField()
+    email = CharField()
+    note = TextField()
+
+    class Meta:
+        database = db
+
+    def full_name(self):
+        """Returns the full (first and last) name of the contact"""
+
+        # print(self.first_name, self.last_name)
+        return "{} {}".format(self.first_name, self.last_name)
+
+
+db.connect()
+db.create_tables([Contact])
